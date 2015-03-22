27bef261e9b8d1f032a81cac01d173ea5dbb5dce
Martin Vidner <martin@vidner.net>
2015-03-22 22:56:16 +0100

    A wrong solution to #8

--- pe0008.hs
+++ pe0008.hs
@@ -1,16 +1,47 @@
+import System.IO
+import Data.Char (isDigit, digitToInt)
+import Data.List.Split
+
+fileContents :: IO String
+fileContents = do
+  handle   <- openFile "pe0008-Largest-product-in-a-series.txt" ReadMode
+  contents <- hGetContents handle
+  print $ lines contents -- why needed??
+  hClose handle
+  return contents
 
 digitString :: IO [Char]
 digitString = do
---  h <- openFile "pe0008-Largest-product-in-a-series.txt"
-  return "123456789123456789" -- fake
+  contents <- fileContents
+  return $ foldl (++) "" $ filter hasDigitsOnly $ lines contents where
+    hasDigitsOnly = all isDigit
+
+spans :: Int -> [a] -> [[a]]
+spans n []     = []
+spans n (x:xs) = (fst $ splitAt n (x:xs)):(spans n xs)
+
+stringProduct :: String -> Int
+stringProduct s = product $ map digitToInt s
 
 -- (theProduct, theDigits) = largestProduct numDigits digitString
 largestProduct :: Int -> [Char] -> (Integer, [Int])
-largestProduct numDigits digitString = (42, [6, 7]) -- fake
+largestProduct numDigits digitString =
+  (fromIntegral $ maximum $ products numDigits digitString , [6,7]) where
+    products n s = map stringProduct $ spans n s
+{-
+  let nonZeroChunks = splitOn "0" digitString,
+      longChunks = filter ((>= numDigits) . length) nonzeroChunks
+
+  longChunks numDigits $ nonZeroChunks digitString
+-}
 
 main :: IO ()
 main = do
+  len <- return 13
   ds <- digitString
-  (product, digits) <- return $ largestProduct 13 ds
+  nonzeroChunks <- return $ splitOn "0" ds
+  longChunks <- return $ filter ((>= len) . length) nonzeroChunks
+  print longChunks
+  (product, digits) <- return $ largestProduct len ds
   putStrLn $ "Product: " ++ show product
   putStrLn $ "Digits:  " ++ show digits
