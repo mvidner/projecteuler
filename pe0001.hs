e2956c085cd9f8c1eb9cc591bed7f4e57a36455e
Martin Vidner <martin@vidner.net>
2015-03-25 21:48:06 +0100

    Used the O(1) solution for #1, parameterized the limit.

--- pe0001.hs
+++ pe0001.hs
@@ -1,3 +1,5 @@
+import System.Environment (getArgs)
+
 divides :: Integral a => a -> a -> Bool
 n `divides` k = k `mod` n == 0
 
@@ -7,5 +9,24 @@ multipleOf3Or5 n = 3 `divides` n || 5 `divides` n
 sumFizzBuzzUnder :: Integral a => a -> a
 sumFizzBuzzUnder n = sum $ filter multipleOf3Or5 [1 .. n-1]
 
+sumOfMultiplesUnder :: Int -> Integer -> Integer
+sumOfMultiplesUnder d n = dd * (intSum m)
+                          where dd       = fromIntegral d
+                                m        = (n - 1) `div` dd
+                                intSum a = a * (a + 1) `div` 2
+
+sumFizzBuzzUnder' n = sumOfMultiplesUnder 3  n +
+                      sumOfMultiplesUnder 5  n -
+                      sumOfMultiplesUnder 15 n
+
+-- helper for benchmarks
+argWithDefault :: Integer -> IO Integer
+argWithDefault s = do
+  args <- getArgs
+  return $ case args of
+    (x:xs) -> read x
+    []     -> s
+
 main = do
-  print $ sumFizzBuzzUnder 1000
+  n <- argWithDefault 1000
+  print $ sumFizzBuzzUnder' n
