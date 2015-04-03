d6df7f64f7a1f4163d6180461513ca53d3c1ddbf
Martin Vidner <martin@vidner.net>
2015-04-04 00:00:00 +0200

    Stated and solved #12
    with a tiny primes library

---
+++ lib/Primes.hs
@@ -0,0 +1,12 @@
+module Primes where
+
+unfaithfulSievePrimes = sieve [2..]
+  where sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+
+-- Melissa O'Neil
+-- in http://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
+trialDivisionPrimes = 2 : [x | x <- [3..], isprime x]
+
+isprime x = all (\p -> x `mod` p > 0) (factorsToTry x)
+  where
+    factorsToTry x = takeWhile (\p -> p*p <= x) trialDivisionPrimes
