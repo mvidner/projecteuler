7f76771c110002084d5cbd98dcc47e22a6b2b1a5
Martin Vidner <martin@vidner.net>
2015-04-04 22:11:46 +0200

    Solved #21.
    Exclude perfect numbers.

--- pe0021.hs
+++ pe0021.hs
@@ -48,11 +48,15 @@ allDivisors n = allDivisorsFromCanonicalRepresentation $
 -- end of BOO
 
 properDivisors :: Integer -> [Integer]
+properDivisors 0 = []
 properDivisors n = filter (/= n) $ allDivisors n
 
 sumOfProperDivisors = sum . properDivisors
 
-isAmicable n = n == (sumOfProperDivisors (sumOfProperDivisors n))
+isAmicable n =
+  let d  = sumOfProperDivisors n
+      dd = sumOfProperDivisors d
+  in (n /= d) && (n == dd)
 
 main = do
   print $ sum $ filter isAmicable [1..9999]
