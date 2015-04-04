0cf81f61b6d99b0a200a2a70af2621135ba83973
Martin Vidner <martin@vidner.net>
2015-04-04 09:12:23 +0200

    Stated and solved #13.

---
+++ pe0013.hs
@@ -0,0 +1,26 @@
+import Data.List
+import Data.Char (isDigit)
+
+main = do
+  sum <- largeSum
+  putStrLn $ firstDigits 10 sum
+
+for = flip map
+
+numbersIn :: String -> [Integer]
+numbersIn contents =
+  for (lines contents) $ \line ->
+    if hasDigitsOnly line
+      then read line :: Integer
+      else 0
+
+hasDigitsOnly s = (all isDigit s) && (s /= "")
+
+largeSum :: IO Integer
+largeSum = do
+  contents <- readFile "pe0013-Large-sum.txt"
+  return $ sum $ numbersIn contents
+
+firstDigits :: Int -> Integer -> String
+firstDigits count bigNumber =
+  take count $ show bigNumber
