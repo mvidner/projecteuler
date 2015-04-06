8277489d71c141b838166c63792f7484571a3f39
Martin Vidner <martin@vidner.net>
2015-04-07 00:25:09 +0200

    Stated and solved #25.

---
+++ src/PE0025.hs
@@ -0,0 +1,10 @@
+module PE0025 where
+
+import Data.List
+fibs = 1 : 1 : ( zipWith (+) fibs (tail fibs))
+
+main = do
+  let mi = findIndex (\n -> 1000 == (length $show n)) fibs
+  let result = case mi of Just i -> show (i+1)
+                          _      -> "Oops"
+  putStrLn result
