a0559581c7c75ba326b14e284e2ecdaeca6ad183
Martin Vidner <martin@vidner.net>
2015-04-07 00:05:07 +0200

    Improved bruteforcing #24.
    32s, 600MB

--- src/PE0024.hs
+++ src/PE0024.hs
@@ -1,6 +1,11 @@
 module PE0024 where
 
-import Data.List (permutations,sort)
+import Data.List (permutations, sort)
+import Data.ByteString.Char8 (pack, unpack)
+
+
+charPermutations = permutations "0123456789"
+sortedPermutations = sort $ map pack charPermutations
 
 main = do
-  putStrLn $ (sort (permutations "0123456789")) !! 999999
+  putStrLn $ unpack $ sortedPermutations !! 999999
