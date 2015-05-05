5377b3952b2b411bfc14e6e0b7320ced747693d3
Martin Vidner <martin@vidner.net>
2015-05-05 17:44:33 +0200

    Cleaned up #8.

--- src/PE0008.hs
+++ src/PE0008.hs
@@ -1,16 +1,15 @@
 module PE0008 where
 
-import System.IO
 import Data.Char (isDigit, digitToInt)
-import Data.List.Split (splitOn)
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+
+-- | Applies a function but returns a pair of (input, output)
+apply :: (a -> b) -> a -> (a, b)
+apply f x = (x, f x)
 
 fileContents :: IO String
-fileContents = do
-  handle   <- openFile "PE0008-Largest-product-in-a-series.txt" ReadMode
-  contents <- hGetContents handle
-  print $ lines contents -- why needed??
-  hClose handle
-  return contents
+fileContents = readFile "PE0008-Largest-product-in-a-series.txt"
 
 getDigitString :: IO [Char]
 getDigitString = do
@@ -25,25 +24,16 @@ spans n (x:xs) = (fst $ splitAt n (x:xs)):(spans n xs)
 stringProduct :: String -> Integer
 stringProduct s = product $ map (fromIntegral . digitToInt) s
 
--- (theProduct, theDigits) = largestProduct numDigits digitString
-largestProduct :: Int -> [Char] -> (Integer, [Int])
+-- (theDigits, theProduct) = largestProduct numDigits digitString
+largestProduct :: Int -> [Char] -> ([Char], Integer)
 largestProduct numDigits digitString =
-  (maximum $ products numDigits digitString , [6,7]) where
-    products n s = map stringProduct $ spans n s
-{-
-  let nonZeroChunks = splitOn "0" digitString,
-      longChunks = filter ((>= numDigits) . length) nonzeroChunks
-
-  longChunks numDigits $ nonZeroChunks digitString
--}
+  maximumBy (comparing snd) $ products numDigits digitString where
+    products n s = map (apply stringProduct) $ spans n s
 
 main :: IO ()
 main = do
-  len <- return 13
+  let len = 13
   ds <- getDigitString
-  nonzeroChunks <- return $ splitOn "0" ds
-  longChunks <- return $ filter ((>= len) . length) nonzeroChunks
-  print longChunks
-  (theProduct, digits) <- return $ largestProduct len ds
+  (digits, theProduct) <- return $ largestProduct len ds
   putStrLn $ "Product: " ++ show theProduct
   putStrLn $ "Digits:  " ++ show digits
